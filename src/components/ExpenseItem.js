ac799e34df60ef890ea8fec622c653fa91961ab1
Jeffrey Ke <jeffke613@gmail.com>
2022-05-01 18:07:35 -0700

    Made the date of the expense items variable.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
   return (
-    <div className='expense-item'>
-      <div>Date</div>
-      <div className='expense-item__description'>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
         <h2>Title</h2>
-        <div className='expense-item__price'>Amount</div>
+        <div className="expense-item__price">Amount</div>
       </div>
     </div>
   );
