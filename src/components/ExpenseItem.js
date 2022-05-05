188fc90407022d28b37a0808a2e9362de3d731ec
Jeffrey Ke <jeffke613@gmail.com>
2022-05-05 09:44:11 -0700

    Created a listener for click events on a button to change the title of the expense tracker.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,14 +3,19 @@ import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
-      </div>
-    </Card>
+    function clickHandler() {
+        console.log('Clicked')
+    }
+
+    return (
+        <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">{props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change Title</button>
+        </Card>
   );
 }
 
