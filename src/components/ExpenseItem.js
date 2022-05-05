17add6a2c4f7f461a638c237b4179423089d30de
Jeffrey Ke <jeffke613@gmail.com>
2022-05-05 13:46:42 -0700

    Added usestate functionality to my ExpenseItem component where it listens for a button click and updates the title of the expense item.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
+import React, { useState } from 'react'
+
 import "./ExpenseItem.css";
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem(props) {
+function ExpenseItem(props) { 
+
+    const [title, setTitle] = useState(props.title)
+
     function clickHandler() {
+        setTitle('Updated');
         console.log('Clicked')
     }
 
@@ -11,7 +17,7 @@ function ExpenseItem(props) {
         <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">{props.amount}</div>
         </div>
         <button onClick={clickHandler}>Change Title</button>
