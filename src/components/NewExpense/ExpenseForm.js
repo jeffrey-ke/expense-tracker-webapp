20bc43abddb3940ba5beab996639deac3951f5c7
Jeffrey Ke <jeffke613@gmail.com>
2022-05-18 18:19:28 -0700

    Added event listeners for all input fields of the new expense item.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './ExpenseForm.css'
 const ExpenseForm = () => {
-    const titleChangeHandler = (event) => {
-        console.log(event.target.value)
-    };
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState(0.00)
+    const [enteredDate, setEnteredDate] = useState(new Date(2003, 11, 11))
+
+    const titleChangeHandler = (event) => setEnteredTitle(event.target.value)
+    const amountChangeHandler = (event) => setEnteredAmount(event.target.value)
+    const dateChangeHandler = (event) => setEnteredDate(event.target.value)
 
     return <form>
         <div className='new-expense__controls'>
@@ -12,12 +16,12 @@ const ExpenseForm = () => {
                 <input type='text' onChange={titleChangeHandler}/>
             </div>
             <div className='new-expense__control'>
-                <label>Title</label>
-                <input type='number' min='0.01' step='0.01' />
+                <label>Amount</label>
+                <input type='number' onChange={amountChangeHandler} min='0.01' step='0.01' />
             </div>
             <div className='new-expense__control'>
-                <label>Title</label>
-                <input type='date' min='1970-01-01' max='2077-12-31' />
+                <label>Date</label>
+                <input type='date' onChange={dateChangeHandler} min='1970-01-01' max='2077-12-31' />
             </div>
         </div>
         <div className='new-expense__Actions'>
