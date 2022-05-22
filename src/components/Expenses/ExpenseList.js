15c697e039a6122e6c9bd28bb198be99afa0d029
Jeffrey Ke <jeffke613@gmail.com>
2022-05-22 13:52:54 -0700

    Instead of using the ternary operator I used &&, which checks the first conditional for true or false, and returns the second parameter if true.

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -10,6 +10,7 @@ function ExpenseList(props) {
 
     const selectYearHandler = (year) => setSelectedYear(year)
 
+    const filteredExpenses = props.expenses.filter((expense) => expense.date.getFullYear().toString() === selectedYear)
     return (
         <div>
             <Card className="expenses">
@@ -17,15 +18,17 @@ function ExpenseList(props) {
                     selected={selectedYear} 
                     onChangeFilter={selectYearHandler} 
                 />
-                {props.expenses.filter((expense) => (
-                    String(expense.date).includes(selectedYear)
-                )).map((expense) => (
-                <ExpenseItem 
-                    key={expense.id}
-                    title={expense.title} 
-                    amount={expense.amount} 
-                    date={expense.date} 
-                />))}
+                {filteredExpenses.length === 0 && <p> No Expenses</p>}
+                {filteredExpenses.length > 0 && (
+                    filteredExpenses.map((expense) => (
+                        <ExpenseItem 
+                            key={expense.id}
+                            title={expense.title} 
+                            amount={expense.amount} 
+                            date={expense.date} 
+                        />
+                    ))
+                )}
             </Card>
         </div>)
 }
